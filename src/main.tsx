9ce99ea85e3d29d58d310b961ff1cc18eb98c454
aashish_k <aashishkathat00@gmail.com>
2024-02-12 21:42:35 +0530

    implemented routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-const router = createBrowserRouter([{path: '/'}])
+import Layout from './Layout.tsx';
+import Home from './components/Home/Home.tsx';
+import './index.css';
+import About from './components/About/About.tsx';
+import Contact from './components/contact/Contact.tsx';
 
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{ path: '', element: <Home /> },
+			{ path: 'about', element: <About /> },
+			{ path: 'contact', element: <Contact /> },
+		],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
